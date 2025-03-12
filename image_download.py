ad9a665674a66f5409693bdc77a34ec70420a03c
zacharyrsmith99 <zachary.r.smith99@gmail.com>
2025-03-12 15:55:27 -0400

    fix: remove mosaic rulesset from wcs

--- image_download.py
+++ image_download.py
@@ -1,7 +1,6 @@
 import requests
 import urllib.parse
 from qgis.core import QgsRasterLayer, QgsProject
-from urllib.error import HTTPError
 import json
 
 # Themes To Services
@@ -16,10 +15,10 @@ themes_to_services = {
 
 default_theme_service = "res05"
 
-#example: full coordinates of pseudo mercator projection
+# Full coordinates of pseudo mercator projection
 whole_world_bbox = "-20037508.3427892,-20037508.3427892,20037508.3427892,20037508.3427892"
 
-#example: mosaic ruleset
+#example mosaic ruleset
 mosaic_rule = {
     "mosaicMethod": "esriMosaicNorthwest",
     "where": "(((UPPER(sub_theme_name) = 'AGRO-ECOLOGICAL ATTAINABLE YIELD ') AND (UPPER(variable) = 'AVERAGE ATTAINABLE YIELD OF BEST OCCURRING SUITABILITY CLASS IN GRID CELL') AND (UPPER(crop) = 'WHEAT') AND (UPPER(water_supply) = 'RAINFED')))",
@@ -28,37 +27,23 @@ mosaic_rule = {
     "mosaicOperation": "MT_FIRST"
 }
 
-#example: rendering rule
+#example rendering rule
 rendering_rule = {
     "rasterFunction": "Suitability and Attainable Yield Symbology",
 }
 
 #example size, this is the max size
 size = "15000,4100"
-
-params = {
-    "f": "image",
-    "bandIds": "",
-    "renderingRule": json.dumps(rendering_rule),
-    "mosaicRule": json.dumps(mosaic_rule),
-    "bbox": whole_world_bbox,
-    "imageSR": "102100",
-    "bboxSR": "102100",
-    "size": size,
-}
-
 output_path = r""
 
 if not output_path:
     raise Exception('Need to input an output path!')
     
-
-
 class GaezImageServer:
     def __init__(self):
         self.base_url = "https://gaez-services.fao.org/server/rest/services/" + default_theme_service + "/ImageServer/exportImage"
     
-    def send_image_download_request(self):
+    def send_image_download_request(self, params):
         encoded_params = urllib.parse.urlencode(params)
         full_url = f"{self.base_url}?{encoded_params}"
         response = requests.get(full_url)
@@ -67,8 +52,9 @@ class GaezImageServer:
             print('Received valid response from GAEZ image server...')
             return response
         
-        raise HTTPError(code=response.status_code, message=f"Failed to send your request to web image server")
-            
+        raise Exception(f"Failed to send your request to WCS server. Received code: ({response.status_code}), {response.content}")            
+
+gaez_image_server = GaezImageServer()
 
 def handle_error(response):
     body = response.json()
@@ -86,7 +72,16 @@ def handle_error(response):
         raise Exception(f"Error code {code} encountered in processing your request." + message + details)
 
 def download_gaez_image():
-    gaez_image_server = GaezImageServer()
+    params = {
+        "f": "image",
+        "bandIds": "",
+        "renderingRule": json.dumps(rendering_rule),
+        "mosaicRule": json.dumps(mosaic_rule),
+        "bbox": whole_world_bbox,
+        "imageSR": "102100",
+        "bboxSR": "102100",
+        "size": size,
+    }
 
     response = gaez_image_server.send_image_download_request()
 
--- wcs_download.py
+++ wcs_download.py
@@ -1,7 +1,6 @@
 import requests
-import json
 from qgis.core import QgsRasterLayer, QgsProject
-from urllib.error import HTTPError
+import xml.dom.minidom
 
 # Themes To Services
 themes_to_services = {
@@ -14,42 +13,16 @@ themes_to_services = {
 }
 default_theme_service = "res05"
 
-#example mosaic ruleset
-mosaic_rule = {
-    "mosaicMethod": "esriMosaicNorthwest",
-    "where": """
-    (((UPPER(sub_theme_name) = 'AGRO-ECOLOGICAL ATTAINABLE YIELD ') 
-    AND 
-        (UPPER(variable) = 'AVERAGE ATTAINABLE YIELD OF BEST OCCURRING SUITABILITY CLASS IN GRID CELL') 
-    AND 
-        (UPPER(crop) = 'WHEAT') AND (UPPER(water_supply) = 'RAINFED')))""",
-    "sortField": "",
-    "ascending": True,
-    "mosaicOperation": "MT_FIRST"
-}
-
-#example rendering rule
-rendering_rule = {
-    "rasterFunction": "Suitability and Attainable Yield Symbology",
-}
-
 WCS_URL = f"https://gaez-services.fao.org/server/services/{default_theme_service}/ImageServer/WCSServer"
 
 COVERAGE_ID = f"{default_theme_service}_Suitability and Attainable Yield Symbology"
 
 min_lon, min_lat, max_lon, max_lat = -179.99999999999997, -89.9999928, 179.9999856, 90
 
-params = {
+base_params = {
     "SERVICE": "WCS",
     "VERSION": "1.0.0",
-    "REQUEST": "GetCoverage",
     "COVERAGE": COVERAGE_ID,
-    "FORMAT": "GeoTIFF",
-    "BBOX": f"{min_lon},{min_lat},{max_lon},{max_lat}",
-    "CRS": "EPSG:4326",
-    "RESOLUTION": "0.08333333", 
-    "mosaicRule": json.dumps(mosaic_rule),
-    "renderingRule": json.dumps(rendering_rule), 
 }
 
 output_path = r""
@@ -75,17 +48,52 @@ class GaezWcsServer:
     def __init__(self):
         self.base_url = WCS_URL
     
-    def send_wcs_request(self):
-        response = requests.get(self.base_url, params=params)
+    def send_wcs_request(self, params):
+        response = requests.get(self.base_url, params)
         if response.status_code == 200:
             print('Received valid response from GAEZ WCS server...')
             return response
-        
-        raise HTTPError(code=response.status_code, message=f"Failed to send your request to WCS server")
+        raise Exception(f"Failed to send your request to WCS server. Received code: ({response.status_code}), {response.content}")
 
-def download_gaez_wcs():
-    gaez_wcs_server = GaezWcsServer()
-    response = gaez_wcs_server.send_wcs_request()
+gaez_wcs_server = GaezWcsServer()
+
+def describe_gaez_coverage():
+    params = {
+        **base_params,
+        "REQUEST": "DescribeCoverage",
+    }
+    
+    response = gaez_wcs_server.send_wcs_request(params)
+    xml_str = response.content.decode("utf-8")
+    pretty_xml = xml.dom.minidom.parseString(xml_str).toprettyxml(indent="  ")
+    
+    print(pretty_xml)
+
+def get_gaez_capabilities():
+    params = {
+        **base_params,
+        "REQUEST": "GetCapabilities",
+    }
+    
+    response = gaez_wcs_server.send_wcs_request(params)
+    xml_str = response.content.decode("utf-8")
+    pretty_xml = xml.dom.minidom.parseString(xml_str).toprettyxml(indent="  ")
+    
+    print(pretty_xml)
+
+def download_image():
+    """
+    Mosaic rules are IGNORED in WCS!
+    """
+    params = {
+        **base_params,
+        "REQUEST": "GetCoverage",
+        "FORMAT": "GeoTIFF",
+        "BBOX": f"{min_lon},{min_lat},{max_lon},{max_lat}",
+        "CRS": "EPSG:4326",
+        "RESOLUTION": "0.08333333",
+    }
+    response = gaez_wcs_server.send_wcs_request(params)
     with open(output_path, 'wb') as f:
         f.write(response.content)
     raster_layer = QgsRasterLayer(output_path, "WCS Coverage")
